c83dad5f8d580451565e194edafeca5296582f2b
s1023347_Moon <s1023347@mail.yzu.edu.tw>
2016-04-11 16:45:20 +0800

    修改Get_Data的參數
    string DataFileName -> StreamReader dataFile

--- MergeDataAndDoc/Program.cs
+++ MergeDataAndDoc/Program.cs
@@ -48,53 +48,49 @@ namespace MergeDataAndDoc
             }
 
             Program p = new Program();
-            p.Get_Data( DataFileName, Var, data );
+            using (StreamReader dataFile = new StreamReader(DataFileName)) // 讀檔案
+                p.Get_Data( dataFile, Var, data );
 
 
 
             using (StreamWriter outFile = new StreamWriter(outputFileName)) // 寫檔案
-            using (StreamReader template = new StreamReader(TemplateFileName)) // 寫檔案
+            using (StreamReader template = new StreamReader(TemplateFileName)) // 讀檔案
                 p.outputFile(outFile, template, Var, data);
-            //p.outputFile(outputFileName, TemplateFileName, Var, data);
+
               
         }
 
-        public void Get_Data(string DataFileName, List<string> Var, List<List<string>> data)
+        public void Get_Data(StreamReader dataFile, List<string> Var, List<List<string>> data)
         {
-            using (StreamReader inputFile = new StreamReader(DataFileName)) // 讀檔案
+            string line;
+            int count = 0;
+            while ((line = dataFile.ReadLine()) != null)
             {
-                string line;
-                int count = 0;
-                while ((line = inputFile.ReadLine()) != null)
+                string tmpVar = "";
+                int index = 0, nextIndex = 0;
+                List<string> listData = new List<string>(); // 存單筆資料(一行)
+
+                while( nextIndex != -1 ) // 讀完那一行
                 {
-                    string tmpVar = "";
-                    int index = 0, nextIndex = 0;
-                    List<string> listData = new List<string>(); // 存單筆資料(一行)
-
-                    while( nextIndex != -1 ) // 讀完那一行
-                    {
-                        nextIndex = line.IndexOf('\t', index+1);
-                        if( nextIndex >= 0 ) // 避免第一行只有一個變數欄位，或是讀到最後一個變數
-                            tmpVar = line.Substring(index, nextIndex-index);
-                        else
-                            tmpVar = line.Substring(index, line.Length-index);
-                        index = nextIndex+1;
-                        while (line[index].Equals('\t'))
-                            index++;
-                        if (count == 0) // 表示第一行，要存變數名稱
-                            Var.Add(tmpVar);
-                        else
-                            listData.Add(tmpVar);
-
-
-                    }
-                    if (count != 0)
-                    {
-                        data.Add(listData);
-                        listData = new List<string>();
-                    }
-                    count++;
+                    nextIndex = line.IndexOf('\t', index+1);
+                    if( nextIndex >= 0 ) // 避免第一行只有一個變數欄位，或是讀到最後一個變數
+                        tmpVar = line.Substring(index, nextIndex-index);
+                    else
+                        tmpVar = line.Substring(index, line.Length-index);
+                    index = nextIndex+1;
+                    while (line[index].Equals('\t'))
+                        index++;
+                    if (count == 0) // 表示第一行，要存變數名稱
+                        Var.Add(tmpVar);
+                    else
+                        listData.Add(tmpVar);
                 }
+                if (count != 0)
+                {
+                    data.Add(listData);
+                    listData = new List<string>();
+                }
+                count++;
             }
         }
 
@@ -104,13 +100,6 @@ namespace MergeDataAndDoc
             string tem = template.ReadToEnd();
             for( int k = 0; k < data.Count; k++ ) // 對每一筆資料(中文姓名/身分證/年數)進行替換
             {
-             /*   while ((line = Tfile.ReadLine()) != null) // 對每一行讀入的template進行替換
-                {
-                    for( int i = 0; i < Var.Count; i++ )
-                        line = line.Replace("${" + Var[i] + "}", data[k][i]);
-                    outFile.WriteLine(line);
-                }
-                Tfile.Close();*/
                 string write = tem;
                 for( int i = 0; i < Var.Count; i++ )
                     write = write.Replace("${" + Var[i] + "}", data[k][i]);
